4add75d6b0001aa772ebbb6e9e45428f76272427
Paul Swartz <paul@paulswartz.net>
2019-12-05 10:38:24 -0500

    chore: bump version to 1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule LoggerSplunkBackend.Mixfile do
   def project do
     [
       app: :logger_splunk_backend,
-      version: "0.0.1",
+      version: "1.0.0",
       elixir: "~> 1.0",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
